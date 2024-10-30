33b3eae2a2bf5916405c8a5ad32f410dd219b4fe
Solaiman Hossain <32815625+Solaiman0168@users.noreply.github.com>
2024-10-30 08:19:18 +0600

    node server init

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
     res.send('Hello from Node Api');
 });
