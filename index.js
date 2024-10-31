61d21c5ddd97a918cc74e8390c4b95251e078b96
Solaiman Hossain <32815625+Solaiman0168@users.noreply.github.com>
2024-10-31 09:09:11 +0600

    nodemon init

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
+// Using ES6 imports
+import mongoose from 'mongoose';
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.get('/', (req, res) => { 
-    res.send('Hello from Node Api');
+    res.send('Hello from Node Api server');
 });
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
