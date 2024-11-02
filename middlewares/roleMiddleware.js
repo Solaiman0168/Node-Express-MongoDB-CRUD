a7b921c549e386ad0d96b9a9e11eeca8f01895fc
Solaiman Hossain <32815625+Solaiman0168@users.noreply.github.com>
2024-11-02 08:30:51 +0600

    comment add

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -2,7 +2,7 @@ const authorizeRoles = (...allowedRoles) => {
     return (req, res, next) => {
         console.log("Allowed roles:", allowedRoles);
         console.log("User role:", req.user?.role);
-        if (!allowedRoles.includes(req.user.role)) {
+        if (!allowedRoles.includes(req.user?.role)) { //req.user.role
             return res.status(403).json({
                 status: "fail",
                 message: "Access denied"
